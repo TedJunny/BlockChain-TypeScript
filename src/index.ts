b1f6051b3456715e9adb07a903e621abbd45f3e6
YeJun <hayejun1013@naver.com>
2021-12-08 16:41:02 +0900

    feat: add typescript index file on studying class type

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const ted = new Human("ted", 24, "male");
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+};
+
+console.log(sayHi(ted));
+
+export {};
